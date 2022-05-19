c16a83938ce92029f5d1f4658da1e9743033b788
mohammedkharoda <kharodawalam@gmail.com>
2022-05-19 07:42:44 +0530

    first commit

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '3d0ae9e5-b217-4c49-b81b-d05daf2e47b5';
+export const KEY = 'c91d077c-e1fc-4448-ab5c-9735239c3227';
 export const MODAL_CLOSE_SEC = 2.5;
